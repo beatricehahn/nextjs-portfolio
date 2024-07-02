3b3e31678f30443ae429dc76bd3153fdc93c9cd5
Beatrice Hahn <beatricehahn7@gmail.com>
2024-07-02 11:47:13 -0400

    Ignore Grid content

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,27 +1,27 @@
-// import React from 'react'
-// import { gridItems } from '@/data'
-// import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import React from 'react'
+import { gridItems } from '@/data'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
 
-// export const Grid = () => {
-//   return (
-//     <section id="work">
-//         <BentoGrid>
-//           {gridItems.map(({ id, title, description, className, img, imgClassName, titleClassName, spareImg}) => (
-//               <BentoGridItem 
-//                 id={id}
-//                 key={id}
-//                 title={title}
-//                 description={description}
-//                 className={className}
-//                 img={img}
-//                 imgClassName={imgClassName}
-//                 titleClassName={titleClassName}
-//                 spareImg={spareImg}
-//               />
-//             ))}
-//         </BentoGrid>
-//     </section>
-//   )
-// }
+export const Grid = () => {
+  return (
+    <section id="work">
+        {/* <BentoGrid>
+          {gridItems.map(({ id, title, description, className, img, imgClassName, titleClassName, spareImg}) => (
+              <BentoGridItem 
+                id={id}
+                key={id}
+                title={title}
+                description={description}
+                className={className}
+                img={img}
+                imgClassName={imgClassName}
+                titleClassName={titleClassName}
+                spareImg={spareImg}
+              />
+            ))}
+        </BentoGrid> */}
+    </section>
+  )
+}
 
-// export default Grid
+export default Grid
