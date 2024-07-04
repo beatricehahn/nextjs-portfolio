cbe180bd9035cd9cb1cd85ccff6e2e579d7f31cb
Beatrice Hahn <beatricehahn7@gmail.com>
2024-07-04 00:34:36 -0400

    Update project data

--- data/index.ts
+++ data/index.ts
@@ -23,27 +23,29 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "UX Screenflow of Educational Mobile Game",
-    des: "Learn the dangers of phishing attacks while playing an educational game",
-    img: "/p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    genre: "Education & Gaming",
+    title: "DIGITAL EXORCIST: A PHISHING GAME",
+    des: "UX screenflow of educational mobile game in development",
+    img: "/images/work-img-placeholder.png",
     link: "/",
+    tags: ["UX", "2024"]
   },
   {
     id: 2,
-    title: "UI Design for Health Monitoring App",
-    des: "Users can monitor their vitals and recieve notifications if they are at risk of fainting",
-    img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    genre: "Health",
+    title: "CONFISTAND",
+    des: "Health monitoring app for users who can monitor their vitals and recieve notifications if they are at risk of fainting",
+    img: "/images/work-img-placeholder.png",
     link: "/",
+    tags: ["UI", "UX", "2023"]
   },
   {
     id: 3,
-    title: "Trojan Dining Menu Web App",
-    des: "A redesigned web app to view all dining hall menus at a glance",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    genre: "Food & Dining",
+    title: "TROJAN DINING",
+    des: "A redesigned web app to view all campus dining hall menus at a glance",
+    img: "/images/work-img-placeholder.png",
     link: "/",
+    tags: ["UI", "Development", "2022"]
   },
-
 ];
