f0ca4b56b0a3c6e37904b7f59c36219bb592d300
Beatrice Hahn <beatricehahn7@gmail.com>
2024-07-05 21:34:13 -0400

    Add about section content placeholders

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,13 @@ export default function About() {
     return (
         <div>
             <h1>ABOUT</h1>
+            <img src="/images/work-img-placeholder.png" alt="Portrait" />
+            <h2>Overview</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione natus nulla dolor adipisci blanditiis velit enim dolore porro, culpa, magnam eius esse? Dignissimos, voluptates est sequi aspernatur distinctio id atque.</p>
+            <h2>My Background</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore quae architecto veritatis dolorem, soluta dolorum asperiores ipsa facere culpa blanditiis omnis officia qui mollitia nostrum inventore incidunt voluptatibus ex nesciunt.</p>
+            <h2>Experience</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum cupiditate numquam voluptatum mollitia alias velit sequi repudiandae quae totam maxime tempore, exercitationem dolor ea excepturi eum distinctio natus ratione earum!</p>
         </div>
     );
 }
