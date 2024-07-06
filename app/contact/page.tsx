7b119533c15f3625b8196828215116060f5b5304
Beatrice Hahn <beatricehahn7@gmail.com>
2024-07-05 21:46:18 -0400

    Add contact page content placeholder

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,6 +2,16 @@ export default function Contact() {
     return (
         <div>
             <h1>CONTACT</h1>
+            <h2>LET'S THINK OUTSIDE THE BOX TOGETHER.</h2>
+            <p>For inquiries or collaboration</p>
+            <span>Email me at hello@beahahn.com</span>
+            <br/>
+            <span>Reach out over on <a href="/">LinkedIn</a></span>
+            <br/>
+            <span>Say hello on <a href="/">Instagram</a></span>
+            <form action="">
+
+            </form>
         </div>
     );
 }
