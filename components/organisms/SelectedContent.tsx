3c60f5e73440ad08dbac4ece94c37b5e30bdf0a3
Beatrice Hahn <beatricehahn7@gmail.com>
2024-07-08 17:05:59 -0700

    Disable gradient temporarily

--- components/organisms/SelectedContent.tsx
+++ components/organisms/SelectedContent.tsx
@@ -5,12 +5,12 @@ import QuoteBlock from "../QuoteBlock"
 
 export const SelectedContent = () => {
   return (
-    <div className=" bg-black-fluid-pattern bg-cover w-screen h-auto">
-      <img src="/images/purplish-gradient-bg.png" alt="" className="absolute w-screen z-[-1] h-auto"/>
-      <RecentProjects />
-      <AboutBlock />
-      <Contact />
-      <QuoteBlock />
-    </div>
+      <div className="bg-black-fluid-pattern bg-cover w-screen h-auto">
+        {/* <img src="/images/purplish-gradient-bg.png" alt="" className=" z-[-1] h-auto"/> */}
+        <RecentProjects />
+        <AboutBlock />
+        <Contact />
+        <QuoteBlock />
+      </div>
   )
 }
