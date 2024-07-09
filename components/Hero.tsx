55304064967f3143c85b57b7e082b71f2aadc9d6
Beatrice Hahn <beatricehahn7@gmail.com>
2024-07-08 18:32:56 -0700

    Adjust text alignment and add button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,21 +5,19 @@ import '../public/styles/Menu.css'
 const Hero = () => {
   return (
     <div id='hero-container' className='overflow-hidden bg-hero rounded-lg'>
-        <div id='hero-content' className='mb-10 relative flex items-center justify-center'>
+        <div id='hero-content' className='mb-10 flex flex-col md:flex-row justify-end'>
           <img 
             src="/images/hero-portrait.png" 
             alt="portrait illustration"
-            className='object-cover w-full h-full md:w-[80%]' 
+            className='object-cover w-[70%] h-[auto] mx-auto' 
           />
-          <div id='hero-header' className='absolute inset-0 flex flex-col items-center justify-center z-10'>
-            <h1 className='font-bold text-white text-6xl md:text-7xl lg:text-8xl'>
+          <div id='hero-header' className='flex flex-col z-10 m-10'>
+            <h1 className='font-bold text-white text-4xl md:text-5xl lg:text-6xl'>
               BEATRICE HAHN
             </h1>
-            <TextGenerateEffect 
-              className='text-center text-[20px]'
-              words="PRODUCT DESIGNER & DEVELOPER"
-            />
-            <p className='text-white'>Specializing in interactive storytelling and motion design</p>
+            <h4 className='text-xl font-bold'>PRODUCT DESIGNER & DEVELOPER</h4>
+            <p className='text-white'>Specializing in visual storytelling and motion design</p>
+            <button className='mt-8 p-2 rounded-md bg-black-100 text-white-200'><a href="#works">View My Work</a></button>
           </div>
         </div>
     </div>
