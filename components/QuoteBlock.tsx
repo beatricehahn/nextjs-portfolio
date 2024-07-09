adf7f21f5d48acd3a3a9547bd2ffa40dcaa9de34
Beatrice Hahn <beatricehahn7@gmail.com>
2024-07-08 17:53:50 -0700

    Adjust spacing

--- components/QuoteBlock.tsx
+++ components/QuoteBlock.tsx
@@ -1,6 +1,6 @@
 const QuoteBlock = () => {
   return (
-    <div className='flex justify-center mt-10 mr-2 ml-2'>
+    <div className='flex justify-center m-10'>
         <blockquote className=''>
             <p className='text-2xl lg:text-4xl text-white-100'>"LIFE PRESENTS ITSELF AS CLAY, AWAITING TO BE SHAPED"</p>
         </blockquote>  
