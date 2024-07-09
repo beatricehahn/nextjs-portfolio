ef743d527775810aa53973844cb4c02ed973e0f4
Beatrice Hahn <beatricehahn7@gmail.com>
2024-07-08 17:05:35 -0700

    Adjust how tags appear on dark bg

--- components/atoms/Tag.tsx
+++ components/atoms/Tag.tsx
@@ -1,6 +1,6 @@
 const Tag = ({ label } : { label : string }) => {
   return (
-    <span className='text-sm rounded-full border-[1.5px] border-white-800 text-white-200 px-4 py-0.5 mr-2 mb-2'>{label}</span>
+    <span className='text-sm rounded-full border-[1.5px] border-white-200 text-white-200 px-4 py-0.5 mr-2 mb-2'>{label}</span>
   )
 }
 
