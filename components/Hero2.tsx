2945901008cc3d750408b9ef27325301df668c71
Beatrice Hahn <beatricehahn7@gmail.com>
2024-09-13 23:17:22 -0700

    Update hero section placeholder

--- components/Hero2.tsx
+++ components/Hero2.tsx
@@ -1,8 +1,8 @@
 export const Hero2 = () => {
   return (
-    <div className='bg-hero h-[180px] w-full p-2 mb-2 rounded-[24px]'>
-        <h4 className='text-xl font-bold'>PRODUCT DESIGNER & DEVELOPER</h4>
-        <p className='text-white text-lg'>Specializing in visual storytelling and motion design</p>
+    <div className='bg-hero h-[180px] w-full p-20 mb-2 rounded-[16px]'>
+        <h4 className='text-xl font-bold'>UX Designer & Developer</h4>
+        <p className='text-white text-lg'>Specializing in visual and interaction design</p>
     </div>
   )
 }
