d0ea84600fc21e1c348940d5fd6fca1791bc9fda
Beatrice Hahn <beatricehahn7@gmail.com>
2024-09-13 23:00:54 -0700

    Update contact section margins

--- components/organisms/Contact.tsx
+++ components/organisms/Contact.tsx
@@ -3,7 +3,7 @@ import CopyButton from "../atoms/CopyButton"
 
 const Contact = () => {
   return (
-    <div id="contact" className='m-10 p-2 border-2 rounded-md border-white-800'>
+    <div id="contact" className='mb-10 p-2 border-2 rounded-md border-white-800'>
       <h1 className="font-bold text-xl md:text-4xl text-purple">Ready to bring your project to life?</h1>
       <p className="text-white-200 text-lg mb-6">Let's think outside the box together.</p>
       <CopyButton link="hello@beahahn.com" />
