f8cfb409cae92c37d6ae728aa8e86126848a58cb
Beatrice Hahn <beatricehahn7@gmail.com>
2024-09-13 23:18:43 -0700

    Update aspect ratio to 4:3

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -3,13 +3,10 @@ import ProjectBox from './molecules/ProjectBox';
 
 const RecentProjects = () => {
   return (
-    <div id="works" className="text-white-100 py-10 m-4">
-        <h1 className="heading text-base md:text-4xl">
-            A selection of {' '}
-            <span className="text-purple">recent works</span>
-        </h1>
+    <div id="works" className="text-white-100 py-10 w-full flex-col justify-center items-center">
+        <h1 className="font-bold text-left text-[20px] md:text-4xl">Selected Work</h1>
         <div
-            className="grid sm:grid-col-1 p-4 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-10">
+            className="grid sm:grid-col-1 md:grid-cols-2 lg:grid-cols-2 md:gap-4 mt-10">
             {projects.map(( { id, genre, title, img, tags }) => (
                 <div key={id} className=''>
                     <ProjectBox 
--- components/molecules/ProjectBox.tsx
+++ components/molecules/ProjectBox.tsx
@@ -4,19 +4,19 @@ const ProjectBox = (
   { genre, title, img, tags } : { genre : string, title : string, img : string, tags : string[] }
 ) => {
   return (
-    <div className=" flex flex-col md:flex-row md:items-start md:justify-between items-center">
+    <div className="my-[32px] md:my-4 flex flex-col md:flex-row md:items-start md:justify-between items-center">
       <div className="w-full">
           <img 
             src={img}
             alt='Project Picture'
-            className="rounded-[24px]"
+            className="rounded-[16px] w-full aspect-[4/3]"
           />
           <div className="m-2 mix-blend-difference">
-            <span className="text-[14px] mb-1 text-white-200">{genre}</span>
-            <h6 className='text-lg font-bold mb-2'>{title}</h6>
-            {tags.map((tag) => (
+            <span className="text-[14px] md:text-[16px] lg:text-[22px] mb-1 text-white-200">{genre}</span>
+            <h6 className='text-[16px] sm:text-[18px] md:text-[20px] lg:text-[28px] font-bold mb-2'>{title}</h6>
+            {/* {tags.map((tag) => (
               <Tag label={tag} />
-            ))}
+            ))} */}
           </div>
       </div>
     </div>
