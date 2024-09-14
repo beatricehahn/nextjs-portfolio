ea51a09932eb90046b1c20d2d36e6079194123d6
Beatrice Hahn <beatricehahn7@gmail.com>
2024-09-14 16:25:21 -0700

    Update margin

--- components/Hero2.tsx
+++ components/Hero2.tsx
@@ -1,6 +1,6 @@
 export const Hero2 = () => {
   return (
-    <div className='bg-hero h-[180px] w-full p-20 mb-2 rounded-[16px]'>
+    <div className='bg-hero h-[180px] w-full p-20 mb-2 rounded-[16px] mt-10'>
         <h4 className='text-xl font-bold'>UX Designer & Developer</h4>
         <p className='text-white text-lg'>Specializing in visual and interaction design</p>
     </div>
