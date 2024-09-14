def5b4e3afae28dca49591bec9e8da0bf3207ef4
Beatrice Hahn <beatricehahn7@gmail.com>
2024-09-14 16:25:02 -0700

    Setup about page scaffolding

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+export default function About() {
+    return (
+        <div className="mx-[24px] md:mx-20">
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ad earum ipsa quibusdam similique, inventore fugit hic, excepturi est velit corrupti consequuntur vel beatae fugiat. Natus quas aut placeat nisi.</p>
+            <h1>Work Experience</h1>
+            <ul>
+                <li>year - work</li>
+                <li>year - work</li>
+                <li>year - work</li>
+            </ul>
+        </div>
+    )
+}
