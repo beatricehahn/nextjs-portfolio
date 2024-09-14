684868910a6e0b9223c766dee9c7a08c0e324f07
Beatrice Hahn <beatricehahn7@gmail.com>
2024-09-13 22:59:29 -0700

    Update footer margin, paddings, links

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -2,24 +2,23 @@ import { FooterItem } from "../molecules/FooterItem"
 
 export const Footer = () => {
   return (
-    <footer className="bg-black w-full pt-2" id="footer-content-wrapper">
-        <div className='flex justify-start m-10'>
+    <footer className="bg-black w-full" id="footer-content-wrapper">
+        <div className='flex justify-start mt-10 mb-10 mx-[24px] md:mx-20'>
           <ul className='mt-[32px] mb-[32px] mr-[32px] w-[50%]' id='navigation-links'>
             <h6 className='text-white font-bold'>MENU</h6>
-            <li className="mt-[12px]"><a href="#works" className='text-white-200'>Works</a></li>
+            <li className="mt-[12px]"><a href="#works" className='text-white-200'>Work</a></li>
             <li className="mt-[12px]"><a href="#about" className='text-white-200'>About</a></li>
             <li className="mt-[12px]"><a href="#contact" className='text-white-200'>Contact</a></li>
           </ul>
           <ul className='mt-[32px] mb-[32px]' id='social-links'>
-            <h6 className='text-white font-bold'>SOCIAL</h6>
-            <li className="mt-[12px]"><a href="https://www.linkedin.com/in/beatrice-hahn/" className='text-white-200'>LinkedIn</a></li>
-            <li className="mt-[12px]"><a href="https://github.com/beatricehahn" className='text-white-200'>Github</a></li>
+            <h6 className='text-white font-bold'>NETWORK</h6>
+            <li className="mt-[12px]"><a href="https://www.linkedin.com/in/beatrice-hahn/" target="_blank" className='text-white-200'>LinkedIn</a></li>
+            <li className="mt-[12px]"><a href="https://github.com/beatricehahn" target="_blank" className='text-white-200'>Github</a></li>
           </ul>
         </div>
-        <div className='mt-[32px] mb-0 flex flex-col md:flex-row justify-center m-10' id='copyright-and-details'>
-            <span className='text-white-800 p-4'>2024 &copy; Beatrice Hahn</span>
-            <span className='text-white-800 p-4'>Product Designer & Developer</span>
-            <span className='text-white-800 p-4'>Portfolio designed in Figma, developed in Next.JS</span>
+        <div className='mt-[32px] mb-0 flex mx-[24px] md:mx-20' id='copyright-and-details'>
+            <span className='text-white-800 pb-4'>2024 &copy; Beatrice Hahn</span>
+            <span className='text-white-800 pl-8 pb-4'>Product Designer & Developer</span>
         </div>
     </footer>
   )
