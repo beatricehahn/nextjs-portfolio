7b92cb7b4f2c33d0610e451a93191dcbb6380f9a
Beatrice Hahn <beatricehahn7@gmail.com>
2024-09-14 17:07:20 -0700

    Scaffold page for web accessibility article

---
+++ app/articles/web-accessibility-and-inequity/page.tsx
@@ -0,0 +1,7 @@
+export default function WebAccessibility() {
+    return (
+        <div className="mx-[24px] md:mx-20">
+            <h1>Left behind in the digital age: How the lack of web accessibility adds to inequity</h1>
+        </div>
+    )
+}
