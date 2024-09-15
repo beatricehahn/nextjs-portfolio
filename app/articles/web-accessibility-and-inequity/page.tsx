a743372ac4739af263a9362c8d884c2fef8d18ed
Beatrice Hahn <beatricehahn7@gmail.com>
2024-09-14 18:10:24 -0700

    Setup article component with content array

--- app/articles/web-accessibility-and-inequity/page.tsx
+++ app/articles/web-accessibility-and-inequity/page.tsx
@@ -1,7 +1,24 @@
+import { writing } from "@/data";
+
 export default function WebAccessibility() {
+    const { title, subtitle, date, content, sources } = writing[0];
+
     return (
         <div className="mx-[24px] md:mx-20">
-            <h1>Left behind in the digital age: How the lack of web accessibility adds to inequity</h1>
+            <h1 className="font-bold">{title}</h1>
+            <h5>{subtitle}</h5>
+            <h6>Written on {date} by Beatrice Hahn</h6>
+            <div>
+                {content.map((paragraph, index) => (
+                    <p key={index} className="my-[24px]">{paragraph}</p>
+                ))}
+            </div>
+            <h1 className="font-bold">Works Cited</h1>
+            <ul>
+                {sources.map((source, index) => (
+                    <li key={index} className="my-[24px]">{source}</li>
+                ))}
+            </ul>
         </div>
     )
-}
+};
