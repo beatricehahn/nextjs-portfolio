11371432bdfb0e93ba56b6638ddde61df8f58638
Beatrice Hahn <beatricehahn7@gmail.com>
2024-09-25 23:26:02 -0700

    Update intro statement

--- components/Hero2.tsx
+++ components/Hero2.tsx
@@ -1,8 +1,9 @@
 export const Hero2 = () => {
   return (
-    <div className='bg-hero h-[180px] w-full p-20 mb-2 rounded-[16px] mt-10'>
-        <h4 className='text-xl font-bold'>UX Designer & Developer</h4>
+    <div className='bg-hero h-[280px] w-full p-20 mb-2 rounded-[16px] mt-10'>
+        <h4 className='text-xl font-bold'>UI/UX Designer & Developer</h4>
         <p className='text-white text-lg'>Specializing in visual and interaction design</p>
+        <p className="text-white">I’m a California-based Product Designer and Front-End Developer with a focus on <strong><u>interaction design</u></strong> and <strong><u>information architecture</u></strong>. I have contributed to a diverse range of projects, including non-profit, healthcare, education, and interactive media.</p>
     </div>
   )
 }
