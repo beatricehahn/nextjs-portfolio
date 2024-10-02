5d850bc6caa5bb1676d96a85fe4bf5a7baa84910
Beatrice Hahn <beatricehahn7@gmail.com>
2024-10-01 23:53:20 -0700

    Update grid

--- components/atoms/IntroCard.tsx
+++ components/atoms/IntroCard.tsx
@@ -2,7 +2,7 @@ export const IntroCard = () => {
   return (
     <div className="flex flex-col p-[8px] items-center">
         <div className="bg-blue-100 w-[48px] h-[48px] mb-4 rounded-[8px]"></div>
-        <div>
+        <div className="w-[30vw] md:w-[80%]">
           <h6 className="text-left text-purple-bright">Descriptive title goes here</h6>
           <p className="text-left text-[10px]">A description placeholder about how this is a core trait of how I approach my work.</p>
         </div>
--- components/molecules/IntroCards.tsx
+++ components/molecules/IntroCards.tsx
@@ -2,7 +2,7 @@ import { IntroCard } from "../atoms/IntroCard"
 
 const IntroCards = () => {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-4 md:gap-4 mt-[24px]">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:gap-4 mt-[24px]">
       <IntroCard />
       <IntroCard />
       <IntroCard />
