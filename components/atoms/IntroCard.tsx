3e33a1638c551b5433dbe78cc37fa28932ca73fa
Beatrice Hahn <beatricehahn7@gmail.com>
2024-10-03 19:01:21 -0700

    Setup default text

--- components/atoms/IntroCard.tsx
+++ components/atoms/IntroCard.tsx
@@ -1,10 +1,15 @@
-export const IntroCard = () => {
+interface CardProps {
+  title?: string;
+  description?: string;
+}
+
+export const IntroCard = ({ title = "Title Here", description = "Description goes here"} : CardProps) => {
   return (
-    <div className="flex flex-col p-[8px] items-center">
+    <div className="p-[8px]">
         <div className="bg-blue-100 w-[48px] h-[48px] mb-4 rounded-[8px]"></div>
-        <div className="w-[30vw] md:w-[80%]">
-          <h6 className="text-left text-purple-bright">Descriptive title goes here</h6>
-          <p className="text-left text-[10px]">A description placeholder about how this is a core trait of how I approach my work.</p>
+        <div className="">
+          <h6 className="md:text-left text-purple-bright text-[16px] md:text-[14px]">{title}</h6>
+          <p className="md:text-left text-[15px] md:text-[13px] pt-[0x]">{description}</p>
         </div>
     </div>
   )
--- components/molecules/IntroCards.tsx
+++ components/molecules/IntroCards.tsx
@@ -3,10 +3,10 @@ import { IntroCard } from "../atoms/IntroCard"
 const IntroCards = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:gap-4 mt-[24px]">
-      <IntroCard />
-      <IntroCard />
-      <IntroCard />
-      <IntroCard />
+      <IntroCard title="Custom" description="Custom description" />
+      <IntroCard title="Custom" description="Custom description"  />
+      <IntroCard title="Custom" description="Custom description"  />
+      <IntroCard title="Custom" description="Custom description"  />
     </div>
   )
 }
