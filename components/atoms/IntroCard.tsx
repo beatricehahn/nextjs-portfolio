09edf9aac17838be70ddd1ae6fcfafc62f0d1964
Beatrice Hahn <beatricehahn7@gmail.com>
2024-10-17 11:39:18 -0700

    Update intro cards

--- components/atoms/IntroCard.tsx
+++ components/atoms/IntroCard.tsx
@@ -5,8 +5,8 @@ interface CardProps {
 
 export const IntroCard = ({ title = "Title Here", description = "Description goes here"} : CardProps) => {
   return (
-    <div className="p-[8px]">
-        <div className="bg-blue-100 w-[48px] h-[48px] mb-4 rounded-[8px]"></div>
+    <div className="p-[8px] flex flex-col justify-center">
+        <div className="bg-blue-100 w-[100px] h-[100px] mb-4 rounded-[8px]"></div>
         <div className="">
           <h6 className="md:text-left text-purple-bright text-[16px] md:text-[14px]">{title}</h6>
           <p className="md:text-left text-[15px] md:text-[13px] pt-[0x]">{description}</p>
--- components/molecules/IntroCards.tsx
+++ components/molecules/IntroCards.tsx
@@ -2,11 +2,11 @@ import { IntroCard } from "../atoms/IntroCard"
 
 const IntroCards = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:gap-4 mt-[24px]">
-      <IntroCard title="Custom" description="Custom description" />
-      <IntroCard title="Custom" description="Custom description"  />
-      <IntroCard title="Custom" description="Custom description"  />
-      <IntroCard title="Custom" description="Custom description"  />
+    <div className="grid grid-cols-2 lg:grid-cols-4 md:gap-4 mt-[24px]">
+      <IntroCard title="Developer" description=" with a tools-agnostic learning mindset and a modular approach to UI" />
+      <IntroCard title="UI/UX Designer" description="inspired by diverse disciplines and empathetic to varied perspectives."  />
+      <IntroCard title="Project Manager" description="leading with a bird’s eye and ground-level view for details." />
+      <IntroCard title="Collaborator" description="skilled in mediation, clear communication, and building rapport with teams, clients, and stakeholders." />
     </div>
   )
 }
