8cd4f8be6786c4026773e16eaa0601a17af8657c
Beatrice Hahn <beatricehahn7@gmail.com>
2024-11-10 12:47:48 -0800

    Update component view

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
 //import Hero from "@/components/Hero";
-import { Hero2 } from "@/components/Hero2";
+import { Hero2 } from "@/components/HomePage/Hero2";
 import IntroCards from "@/components/molecules/IntroCards";
-import { SelectedContent } from "@/components/organisms/SelectedContent";
-
-import { Simple } from "@/components/animations/sample-animation";
+import { SelectedContent } from "@/components/HomePage/SelectedContent";
+//import { Simple } from "@/components/animations/sample-animation";
 
 export default function Home() {
   return (
     <main className="max-w-screen-lg mx-auto px-[18px]">
       <Hero2 />
-      {/* <IntroCards /> */}
-      <div className="w-full h-[300px]">
+      <IntroCards />
+      {/* <div className="w-full h-[300px]">
         <Simple />
-      </div>
+      </div> */}
       <SelectedContent />
     </main>
   );
