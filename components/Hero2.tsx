dc6623b03804a1bca3abed9f6e8a09c1314ee838
Beatrice Hahn <beatricehahn7@gmail.com>
2024-11-10 12:44:35 -0800

    Relocate components to HomePage folder

--- components/Hero2.tsx
+++
@@ -1,8 +0,0 @@
-export const Hero2 = () => {
-  return (
-    <div className='bg-blue-100 w-full p-10 mb-2 rounded-[16px] mt-10'>
-        <h1 className='text-2xl md:text-3xl pb-[24px]'>Shaping Ideas Into Reality Through Design and Development</h1>
-        <p className="text-sm md:text-base">I’m a California-based <strong>Product Designer</strong> and <strong>Front-End Developer</strong> specializing in <strong><u>motion design</u></strong> for scalable, engaging solutions.</p>
-    </div>
-  )
-}
