e7abb21d3004c9f9b86f2832dc00b1d47efc7936
Beatrice Hahn <beatricehahn7@gmail.com>
2024-11-11 12:18:19 -0800

    Update MVP UI of intro cards

--- components/atoms/IntroCard.tsx
+++ components/atoms/IntroCard.tsx
@@ -1,15 +1,16 @@
 interface CardProps {
   title?: string;
   description?: string;
+  bgColor?: string;
 }
 
-export const IntroCard = ({ title = "Title Here", description = "Description goes here"} : CardProps) => {
+export const IntroCard = ({ title = "Title Here", description = "Description goes here", bgColor = "bg-blue-800"} : CardProps) => {
   return (
-    <div className="p-[8px] flex flex-col justify-center">
-        <div className="bg-blue-100 w-[100px] h-[100px] mb-4 rounded-[8px]"></div>
-        <div className="">
-          <h6 className="md:text-left text-purple-bright text-[16px] md:text-[14px]">{title}</h6>
-          <p className="md:text-left text-[15px] md:text-[13px] pt-[0x]">{description}</p>
+    <div className="p-[8px]">
+        {/* <div className="bg-blue-100 w-[100px] h-[100px] mb-4 rounded-[8px]"></div> */}
+        <div className={`${bgColor} p-4 rounded-[16px] md:h-32 lg:h-44`}>
+          <h5 className="md:text-left text-xl">{title}</h5>
+          <p className="md:text-left pb-0 font-sans text-base">{description}</p>
         </div>
     </div>
   )
--- components/molecules/IntroCards.tsx
+++ components/molecules/IntroCards.tsx
@@ -2,11 +2,11 @@ import { IntroCard } from "../atoms/IntroCard"
 
 const IntroCards = () => {
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-4 md:gap-4 mt-[24px]">
-      <IntroCard title="Tools-agnostic Developer:" description="with a modular approach to coding" />
-      <IntroCard title="Data-driven UI/UX Designer:" description="mindful of balancing business and user needs" />
-      <IntroCard title="Adaptable Project Manager:" description="Monitoring progress above and hunting hidden risks below" />
-      <IntroCard title="Enthusiastic Collaborator:" description="ready to integrate my ideas with team solutions" />
+    <div className="grid auto-rows-fr grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-4">
+      <IntroCard title="Tools-agnostic Developer:" description="With a modular approach to coding" bgColor="bg-special-candyfloss"/>
+      <IntroCard title="Data-driven UI/UX Designer:" description="Mindful of balancing business and user needs" bgColor="bg-special-mojitofreeze"/>
+      <IntroCard title="Adaptable Project Manager:" description="Monitoring progress above and hunting hidden risks below" bgColor="bg-special-lavanderwhip"/>
+      <IntroCard title="Enthusiastic Collaborator:" description="Ready to integrate my ideas with team solutions" bgColor="bg-special-coldsteel"/>
     </div>
   )
 }
