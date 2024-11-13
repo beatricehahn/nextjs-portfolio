9cb2c9ac7359a7a8eeb7fac12348fa45ec5e3917
Beatrice Hahn <beatricehahn7@gmail.com>
2024-11-12 22:47:18 -0800

    Update tag styling

--- components/atoms/Tag.tsx
+++ components/atoms/Tag.tsx
@@ -1,6 +1,6 @@
 const Tag = ({ label } : { label : string }) => {
   return (
-    <span className='text-sm rounded-full border-[1.5px] border-white-200 text-white-200 px-4 py-0.5 mr-2 mb-2'>{label}</span>
+    <span className='text-xs rounded-[4px] border-[1.5px] border-blue-800 text-blue-800 px-4 py-0.5 mr-2 mb-2'>{label}</span>
   )
 }
 
