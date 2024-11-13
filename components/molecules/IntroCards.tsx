e57fe73eae26c11bd7adab886094b522f0e78290
Beatrice Hahn <beatricehahn7@gmail.com>
2024-11-12 22:47:35 -0800

    Update intro cards with new content

--- components/molecules/IntroCards.tsx
+++ components/molecules/IntroCards.tsx
@@ -3,10 +3,10 @@ import { IntroCard } from "../atoms/IntroCard"
 const IntroCards = () => {
   return (
     <div className="grid auto-rows-fr grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-4">
-      <IntroCard title="Tools-agnostic Developer:" description="With a modular approach to coding" bgColor="bg-special-candyfloss"/>
-      <IntroCard title="Data-driven UI/UX Designer:" description="Mindful of balancing business and user needs" bgColor="bg-special-mojitofreeze"/>
-      <IntroCard title="Adaptable Project Manager:" description="Monitoring progress above and hunting hidden risks below" bgColor="bg-special-lavanderwhip"/>
-      <IntroCard title="Enthusiastic Collaborator:" description="Ready to integrate my ideas with team solutions" bgColor="bg-special-coldsteel"/>
+      <IntroCard title="Tools-agnostic Developer:" description="With a modular approach to coding" bgColor="bg-blue-100"/>
+      <IntroCard title="Data-driven UI/UX Designer:" description="Mindful of balancing business and user needs" bgColor="bg-blue-100"/>
+      <IntroCard title="Adaptable Project Manager:" description="Monitoring progress above and hunting hidden risks below" bgColor="bg-blue-100"/>
+      <IntroCard title="Enthusiastic Collaborator:" description="Ready to integrate my ideas with team solutions" bgColor="bg-blue-100"/>
     </div>
   )
 }
