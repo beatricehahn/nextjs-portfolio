2cfb07899a65b4331340ea098c2eb7fc6f538680
Beatrice Hahn <beatricehahn7@gmail.com>
2024-11-30 16:05:35 -0800

    Temporary intro card relocation

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 //import Hero from "@/components/Hero";
 import { Hero2 } from "@/components/HomePage/Hero2";
-import IntroCards from "@/components/molecules/IntroCards";
 import { SelectedContent } from "@/components/HomePage/SelectedContent";
 //import { Simple } from "@/components/animations/sample-animation";
 
@@ -8,7 +7,6 @@ export default function Home() {
   return (
     <main className="max-w-screen-lg mx-auto px-[18px]">
       <Hero2 />
-      <IntroCards />
       {/* <div className="w-full h-[300px]">
         <Simple />
       </div> */}
