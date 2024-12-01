65cdadd309913ac7a89864c014dd6b9583b885f8
Beatrice Hahn <beatricehahn7@gmail.com>
2024-11-30 16:07:38 -0800

    Update intro summary

--- components/HomePage/Hero2.tsx
+++ components/HomePage/Hero2.tsx
@@ -1,8 +1,8 @@
 export const Hero2 = () => {
   return (
-    <div className='w-full p-10 mb-2 rounded-[16px] mt-10'>
-        <h1 className='text-2xl md:text-3xl pb-[24px]'>Adapting data-driven narratives into designs for clear use and understanding </h1>
-        <p className="text-sm md:text-base">I’m a California-based <strong>Digital Product Designer</strong> with an background in programming, human computer interaction research, and project management.</p>
+    <div className='w-full mb-2 rounded-[16px] mt-10'>
+        <h1 className='text-2xl md:text-4xl'>Adapting data-driven narratives into business and user solutions</h1>
+        <p className="text-base md:text-lg">I’m a California-based <strong>Digital Product Designer</strong> with a background in programming, human computer interaction research, and project management.</p>
     </div>
   )
 }
